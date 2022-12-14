558ce2d82ecf3fe6055907348ee67f851945c6f5
Cristina Márquez <cristinamarqizq@gmail.com>
2022-12-14 09:36:04 +0100

    update booking

--- boating-app-frontend/src/logic/updateBooking.js
+++ boating-app-frontend/src/logic/updateBooking.js
@@ -5,7 +5,6 @@ async function updateBooking(bookingId, bookingInfo) {
     const response = await axios.patch(`http://localhost:8080/bookings/${bookingId}`, bookingInfo)
     return response.data
 
-
 }
 
 export default updateBooking
